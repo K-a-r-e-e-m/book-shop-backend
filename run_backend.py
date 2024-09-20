e100cb1655d4d47cda15e6f29704e7966ab5156b
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-09-20 07:17:25 +0300

    Add run backend for deployment

---
+++ run_backend.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+from backend import create_app
+from backend.models.book import load_book_data
+
+"""
+Script to run the Flask application or load initial book data.
+
+Usage:
+- `python run_backend.py`: Runs the Flask application in debug mode.
+- `python run_backend.py load`: Loads initial book data into the database.
+
+Dependencies:
+- `sys`: For command-line argument parsing.
+- `backend`: For creating the Flask application and loading book data.
+
+Functions:
+- `main() -> None`: Main function to handle command-line arguments and run the application.
+
+    Command-line Arguments:
+    - `load`: If provided, the script will execute `load_book_data()` to load book data into the database.
+    - Otherwise, it will run the Flask application.
+
+If executed as the main module, `main()` is called to determine the appropriate action.
+"""
+
+app = create_app()
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == 'load':
+        load_book_data()
+    else:
+        app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    main()
